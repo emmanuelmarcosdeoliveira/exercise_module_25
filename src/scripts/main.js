7e89495938ce1021848937d6e9594d834a0e454f
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-01-22 17:26:23 -0300

    :cosntruction: exercise in progress

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
-console.log("Olá Babel");
+const alunos = ["Emmanuel"];
+const meusAlunos = alunos.map(function (nomeAlunos) {
+  return {
+    nome: nomeAlunos,
+    nota: 7,
+  };
+});
+
+meusAlunos.push({
+  nome: "Everton",
+  nota: 4,
+});
+
+meusAlunos.push({
+  nome: "João",
+  nota: 9,
+});
+
+meusAlunos.push({
+  nome: "Elisangela",
+  nota: 5,
+});
+console.log(meusAlunos);
+
+function filtrAlunos(minhasNotas) {
+  return minhasNotas.nota >= 6;
+}
+
+const notasAcimadeSeis = meusAlunos.filter(filtrAlunos);
+console.log(notasAcimadeSeis);
